f77cf7c07f501b5fed1db08d4bf54d85d299d6f9
Natalie Blumen <natalie.blumen@gmail.com>
2024-10-27 11:03:24 -0400

    october 27 updated pokemon API

--- October2024/Oct26/main.js
+++ October2024/Oct26/main.js
@@ -1,18 +1,30 @@
 //Example fetch using pokemonapi.co
+const pokemonImage = document.getElementById("pokemon-image");
+let cryAudio;
+
 document.querySelector('button').addEventListener('click', getFetch)
+pokemonImage.addEventListener("click", getSound);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch(){;
+    const choice = document.querySelector('input').value
+    const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        pokemonImage.src = data.sprites.other["official-artwork"].front_default;
+        cryAudio = new Audio(data.cries.latest);
         console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+function getSound(){
+    pokemonImage.addEventListener("click", () => {
+        cryAudio.currentTime = 0;
+        cryAudio.play();
+    });
 }
 
 const nasaAPIKey = "VIoGeBwW88QfLjL1WrUJT3GKRQLiGq9Qrs8owN6u";
