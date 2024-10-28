f64e3110f80a900358c172d14e8fff3ab086b754
Natalie Blumen <natalie.blumen@gmail.com>
2024-10-28 14:57:39 -0400

    october 28

--- October2024/Oct26/main.js
+++ October2024/Oct26/main.js
@@ -31,17 +31,30 @@ const nasaAPIKey = "VIoGeBwW88QfLjL1WrUJT3GKRQLiGq9Qrs8owN6u";
 const nasaButton = document.getElementById("nasa-button");
 const nasaData = document.getElementById("nasa-data");
 const nasaImage = document.getElementById("nasa-image");
+const nasaVideo = document.getElementById("nasa-video");
 const nasaDate = document.getElementById("nasa-date");
-const nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=${nasaAPIKey}`;
+const dateChoice = document.getElementById("date-choice")
+let nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=${nasaAPIKey}&date=${dateChoice.value}`;
 
 nasaButton.addEventListener("click", getData);
+
 function getData(){
-    fetch(nasaUrl)
+    console.log(dateChoice.value);
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaAPIKey}&date=${dateChoice.value}`)
     .then(res => res.json())
     .then(data => {
+        if(data.media_type === "image"){
+            nasaVideo.src = "";
+            nasaImage.src = data.hdurl;
+        } else if(data.media_type === "video"){
+            nasaImage.src = "";
+            nasaVideo.src = data.url;
+        } else {
+            console.log("no image or video");
+        }
         nasaDate.textContent = data.date;
         nasaData.textContent = data.explanation;
-        nasaImage.src = data.hdurl;
+        
         nasaImage.alt = data.copyright;
         console.log(data);
     })
