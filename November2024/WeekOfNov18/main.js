b55c1125748270fc7fd4634d33f4a44bd17e1b56
Natalie Blumen <natalie.blumen@gmail.com>
2024-11-20 15:22:24 -0500

    nov 20

--- November2024/WeekOfNov18/main.js
+++ November2024/WeekOfNov18/main.js
@@ -24,3 +24,26 @@ function squareDigits(num){
 
 const thisNum = 54323;
 console.log(squareDigits(thisNum));
+
+function minimumSteps(numbers, value){
+    if(numbers.length === 0 || value < 0) return 0;
+
+    numbers.sort((a,b) => a - b);
+
+    let sum = 0;
+    
+
+    for (let steps = 0; steps < numbers.length; steps++){
+        sum += numbers[steps];
+
+        if(sum >= value){
+            return steps;
+        } 
+    }
+    return numbers.length;
+
+  }
+
+const thisArray = [2,1,2,9,10,22,4];
+const aNumber = 12;
+console.log(minimumSteps(thisArray, aNumber));
