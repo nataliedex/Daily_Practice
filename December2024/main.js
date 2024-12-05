7f846d9d6c001faf13d4599eaf73334a20c973f0
Natalie Blumen <natalie.blumen@gmail.com>
2024-12-05 07:31:01 -0500

    dec 5

--- December2024/main.js
+++ December2024/main.js
@@ -1,12 +1,39 @@
 function disemvowel(str) {
     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-    return str.split("").filter((el) => !vowels.includes(el)).join("");
-        
-
-
-    
+    return str.split("").filter((el) => !vowels.includes(el)).join("");    
   }
 
   const myString = "help they are taking away my vowels!"
   console.log(disemvowel(myString));
+
+function greet(name){
+  //your code here "Hello, <name> how are you doing today?".
+  return `Hello, ${name} how are you doing today?`;
+} 
+
+const myName = "Natalie";
+console.log(greet(myName));
+
+function persistence(num) {
+  let tries = 0;
+
+  while (num.toString().length > 1){
+    num = num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc * cur, 1);
+    tries++;
+  }
+  return tries;
+}
+
+const myNum = 49;
+console.log(persistence(myNum));
+
+function abbrevName(name){
+  return `${name.split(" ")[0][0].toUpperCase()}.${name.split(" ")[1][0].toUpperCase()}`;
+}
+
+const myFullName = "Natalie Blumen";
+console.log(abbrevName(myFullName));
