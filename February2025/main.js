d4075180d26c2123864536ec60084b979b6ed68c
Natalie Blumen <natalie.blumen@gmail.com>
2025-02-02 09:19:17 -0500

    feb2

--- February2025/main.js
+++ February2025/main.js
@@ -5,3 +5,17 @@ function boolToWord( bool ){
 const value = true;
 console.log(boolToWord(value), "Yes");
 
+function removeSmallest(numbers) {
+    if (numbers.length === 0) return [];
+
+    let minValue = Math.min(...numbers);
+    let location = numbers.findIndex(num => num === minValue);
+
+    return numbers.slice(0, location).concat(numbers.slice(location + 1));
+  }
+
+const myArray1 = [1,2,3,4,5];
+const myArray2 = [5,3,2,1,4];
+
+console.log(removeSmallest(myArray1), "[2,3,4,5]");
+console.log(removeSmallest(myArray2), "[5,3,2,4]");
