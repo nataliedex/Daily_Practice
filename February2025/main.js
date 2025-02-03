3d8b06536f0ec94790b073a07be66945cd73ee1a
Natalie Blumen <natalie.blumen@gmail.com>
2025-02-03 09:20:55 -0500

    feb3

--- February2025/main.js
+++ February2025/main.js
@@ -19,3 +19,23 @@ const myArray2 = [5,3,2,1,4];
 
 console.log(removeSmallest(myArray1), "[2,3,4,5]");
 console.log(removeSmallest(myArray2), "[5,3,2,4]");
+
+function bouncingBall(h,  b,  w) {
+    if(h <= 0 || (b <= 0 || b >= 1) || w >= h ){
+        return -1;
+    }
+    let views = 1;
+    h *= b;
+
+    while(h > w) {
+        views += 2;
+        h *= b;
+    }
+    return views;
+  }
+
+const myHeight = 3;
+const myBounce = 0.66;
+const myWindow = 1.5;
+
+console.log(bouncingBall(myHeight, myBounce, myWindow), "3");
