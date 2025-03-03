3ed160c37f3acdaa5f1716fb03c9969cf3246068
Natalie Blumen <natalie.blumen@gmail.com>
2025-03-03 13:47:35 -0500

    mar3

--- March2025/main.js
+++ March2025/main.js
@@ -23,3 +23,45 @@ function accum(s) {
 }
 
 console.log(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu" );
+
+  function howManyTimes(time1, time2) {
+    let start = new Date(time1);
+    let end = new Date(time2);
+    
+    let strikes = 0;
+    let currentStrikes = 0;
+
+    let hours = start.getHours() % 12 || 12; 
+    let minutes = start.getMinutes();
+    let seconds = start.getSeconds();
+
+    if(minutes === 0 && seconds > 0 && seconds <= hours){
+        currentStrikes = hours - seconds + 1;
+    }
+    
+    while (start < end) {
+        let hours = start.getHours() % 12 || 12; 
+        let minutes = start.getMinutes();
+        let seconds = start.getSeconds();
+
+        if (minutes === 0 && seconds === 0) {
+            currentStrikes = hours; 
+        }
+
+        if (minutes === 30 && seconds === 0) {
+            currentStrikes = 1; 
+        }
+
+
+        if (currentStrikes > 0) {
+            strikes++;
+            currentStrikes--; 
+        }
+
+        start.setSeconds(start.getSeconds() + 1);
+    }
+    
+    return strikes;
+}
+
+console.log(howManyTimes("2016-08-14 12:00:00","2016-08-14 12:00:01"), "1");
